2e65545dc0ffb0f4d8afe894017dd552112eb570
Dr_AlienTech <rdatch101@gmail.com>
2019-12-23 14:50:41 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from "firebase";
 
 firebase.initializeApp({
     apiKey: "AIzaSyBVT22t-x2H76119AHG8SgPU0_A0U-N1uA",
