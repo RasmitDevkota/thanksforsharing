fc5e18050fbed09ee6b8280b825e9cfbc6dbabe1
Dr_AlienTech <rdatch101@gmail.com>
2019-12-23 15:02:12 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const app = firebase.initializeApp({
+const firebase = firebase.initializeApp({
     apiKey: "AIzaSyBVT22t-x2H76119AHG8SgPU0_A0U-N1uA",
     authDomain: "my-scrap-project.firebaseapp.com",
     databaseURL: "https://my-scrap-project.firebaseio.com",
