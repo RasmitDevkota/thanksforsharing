d54e6f50b415b7aa7db9227de90cdc0a763f51cc
Dr_AlienTech <rdatch101@gmail.com>
2019-12-24 11:48:43 -0500

    Gitacora saved 2 changed files

--- public/auth.js
+++ public/auth.js
@@ -1,38 +1,3 @@
-firebase.initializeApp({
-    apiKey: "AIzaSyBVT22t-x2H76119AHG8SgPU0_A0U-N1uA",
-    authDomain: "my-scrap-project.firebaseapp.com",
-    databaseURL: "https://my-scrap-project.firebaseio.com",
-    projectId: "my-scrap-project",
-    storageBucket: "my-scrap-project.appspot.com",
-    messagingSenderId: "334998588870",
-    appId: "1:334998588870:web:6b218e9655ade3a6c536c7",
-    measurementId: "G-66W8QQ9W35"
-});
-
-var user = firebase.auth().currentUser;
-var db = firebase.firestore();
-db.enablePersistence();
-
-function redirect(pagePath) {
-    if (pagePath === "signout") {
-        firebase.auth().signOut();
-        window.location.replace("https://www.thx4sharing.web.app");
-    } else {
-        // var urlParams = new URLSearchParams(window.location.search);
-        // var mode = urlParams.get('mode').toString();
-
-        window.location = "https://www.thx4sharing.web.app/" + pagePath;
-    };
-};
-
-window.onload = function () {
-    // var urlParams = new URLSearchParams(window.location.search);
-    // var mode = urlParams.get("darkmode");
-    // if (mode == "dark") {
-    //     document.getElementByTagName("html").id = "dark";
-    // }
-};
-
 // Email Login
 function displayName() {
     var uid = user.uid;
