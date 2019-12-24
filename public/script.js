73251c2429d6c27e1517c4080dbd2084d4b0e958
Dr_AlienTech <rdatch101@gmail.com>
2019-12-24 11:34:41 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -30,6 +30,7 @@ function view(memeid) {
     productRef.onSnapshot(function (doc) {
         var views = doc.data().views;
         var rating = doc.data().rating;
+        
         var memeRef = doc.data().id.toString();
         console.log(views);
     });
