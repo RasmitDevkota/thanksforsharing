5be45f489677535956059ba225aebbf749d37e8b
Dr_AlienTech <rdatch101@gmail.com>
2019-12-24 11:46:02 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -23,7 +23,7 @@ function redirect(pagePath) {
         // var urlParams = new URLSearchParams(window.location.search);
         // var mode = urlParams.get('mode').toString();
 
-        window.location = "https://www.thx4sharing.web.app;
+        window.location = "https://www.thx4sharing.web.app/;
     };
 };
 
