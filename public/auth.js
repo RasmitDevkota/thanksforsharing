5eaab36383ab3afb4f68737caab7856736bedbc8
Dr_AlienTech <rdatch101@gmail.com>
2019-12-24 11:55:55 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -63,7 +63,7 @@ document.addEventListener('keydown', function (event) {
     var password = document.getElementById('password').value;
     var username = document.getElementById('username').value;
     const key = event.key;
-    if (key == "Enter") {
+    if (key == "Enter" && password) {
         eToggleSignIn();
     }
 });
