fb9cee48b3e5a7c61b728f6fc7480df468f6bc29
Dr_AlienTech <rdatch101@gmail.com>
2019-12-24 16:56:20 -0500

    Gitacora saved 3 changed files

--- public/auth.js
+++ public/auth.js
@@ -36,7 +36,7 @@ function eToggleSignIn() {
                 .then(function () {
                     firebase.auth().onAuthStateChanged(function (user) {
                         close("email");
-                            document.getElementById("signin").textContent = "Sign Out";
+                        document.getElementById("signin").textContent = "Sign Out";
                     });
                 })
                 .catch(function (error) {
