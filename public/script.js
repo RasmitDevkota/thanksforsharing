3ff54ba583f2ccc33424b64808d193f21c9f23da
Dr_AlienTech <rdatch101@gmail.com>
2019-12-24 11:33:50 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -25,7 +25,7 @@ window.onload = function () {
 };
 
 function view(memeid) {
-    var memeReference = Memes.doc(memeid);
+    var memeReference = Products.doc(memeid);
 
     memeReference.onSnapshot(function (doc) {
         var views = doc.data().views;
