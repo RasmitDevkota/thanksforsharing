ab9d724093bd6231d603d90ee353dfe820ecbc1b
Dr_AlienTech <rdatch101@gmail.com>
2019-12-24 11:31:46 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -1,11 +1,4 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyCxRTkWjoToUoNsM8Rm6zPwiJBG_JCB4fo",
-    authDomain: "verycoolthanksforsharing.firebaseapp.com",
-    databaseURL: "https://verycoolthanksforsharing.firebaseio.com",
-    projectId: "verycoolthanksforsharing",
-    storageBucket: "verycoolthanksforsharing.appspot.com",
-    messagingSenderId: "78626384450",
-    appId: "1:78626384450:web:5c1a5470485502ab"
 });
 
 var user = firebase.auth().currentUser;
