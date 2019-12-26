0e3b3f6381961f7b3acddb41c611a436bc86a323
Dr_AlienTech <rdatch101@gmail.com>
2019-12-26 09:14:09 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -37,3 +37,12 @@ function log(text) {
     console.log(text);
 };
 
+function signIn() {
+    document.getElementById('popupsignin').style.display = "block";
+};
+function open(elem) {
+    document.getElementById(elem).style.display = "block";
+};
+function close(elem) {
+    document.getElementById(elem).style.display = "none";
+};
