96d41483e3ae39c483410c0a2591ba07154c26b5
Dr_AlienTech <rdatch101@gmail.com>
2019-12-26 08:44:09 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -284,7 +284,7 @@ function sendPasswordReset() {
             } else if (errorCode == 'auth/user-not-found') {
                 alert(errorMessage);
             }
-            console.log(error);
+            log(error);
         });
     } else {
         alert("Please enter an email.");
