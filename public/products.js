4568b9ebc78da66abd24a8a0d9772130d091402f
Dr_AlienTech <rdatch101@gmail.com>
2019-12-25 21:26:35 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -71,7 +71,7 @@ function rate(productid, val) {
     Products.doc(productid).update({
         ratings: firebase.firestore.FieldValue.arrayUnion(val)
     }).then(function(doc){
-        
+        var 
     });
 };
 
