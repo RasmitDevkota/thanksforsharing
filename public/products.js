2e0ef34baf2bcb7aae88a3f305f81d83b630b895
Dr_AlienTech <rdatch101@gmail.com>
2019-12-25 21:31:39 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -62,7 +62,7 @@ function productRedirect(id) {
 };
 
 function addToCart(productid) {
-    
+    var price = 
     cart.update({
         items: firebase.firestore.FieldValue.arrayUnion(productid),
         itemCnt: firebase.firestore.FieldValue.increment(1),
