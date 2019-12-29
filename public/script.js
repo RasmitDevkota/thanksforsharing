0461784c3d409a7a48827e80ffd6a9b0895a1b7d
Dr_AlienTech <rdatch101@gmail.com>
2019-12-29 17:42:29 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -30,11 +30,6 @@ function redirect(pagePath) {
 };
 
 window.onload = function () {
-    // var urlParams = new URLSearchParams(window.location.search);
-    // var mode = urlParams.get("darkmode");
-    // if (mode == "dark") {
-    //     document.getElementByTagName("html").id = "dark";
-    // }
 };
 
 function log(text) {
