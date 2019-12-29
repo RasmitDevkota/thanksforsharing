543f661179a301e278293d64f8c17a883408216d
Dr_AlienTech <rdatch101@gmail.com>
2019-12-29 16:13:44 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -57,7 +57,7 @@ function filter(field) {
             document.getElementById("results").appendChild(outerDiv);
             outerDiv.id = "outer" + name;
 
-            var product = document.createElement("p");
+            var text = document.createElement("p");
             product.innerHTML = name;
             product.className = "product";
             product.onclick = function () {
