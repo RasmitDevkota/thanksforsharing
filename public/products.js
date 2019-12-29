03aa3e9ddd22f39f1acab72f0dad829b82414b1c
Dr_AlienTech <rdatch101@gmail.com>
2019-12-29 13:09:44 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -43,7 +43,7 @@ function view(productid) {
 };
 
 function filter(field) {
-    r
+    document.get
     Products.orderBy(field).get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             var name = doc.data().name.toString();
