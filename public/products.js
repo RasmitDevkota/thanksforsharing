e616a992b1c572fed685b3982c77347e95f43284
Dr_AlienTech <rdatch101@gmail.com>
2019-12-29 16:19:44 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -65,7 +65,7 @@ function filter(field) {
             var name = document.createElement("v-name");
             name.innerHTML = name;
             name.className = "vname";
-            document.getElementById(outerDiv.id).appendChild(product);
+            document.getElementById(test.id).appendChild(product);
 
             var description = document.createElement("v-description");
             description.innerHTML = description;
