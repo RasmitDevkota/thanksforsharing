65e473c0474389b525b6fd82b50d564b49a97661
Dr_AlienTech <rdatch101@gmail.com>
2019-12-29 16:31:46 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -59,7 +59,7 @@ function filter(field) {
             document.getElementById("results").appendChild(outerDiv);
             outerDiv.id = "outer" + name;
 
-            var name = document.createElement("v-name");
+            var image = document.createElement("v-name");
             image.innerHTML = name;
             name.className = "vname";
             document.getElementById(outerDiv.id).appendChild(product-image);
