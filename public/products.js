6be6b1508a03d0ae58e278f89870df3d4d8a2bbf
Dr_AlienTech <rdatch101@gmail.com>
2019-12-30 10:23:03 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -68,6 +68,12 @@ var txtElements = ["name", "description", "price", "rating"];
 var actionElements = ["addtocart", "checkout"];
 var actionNames = ["Add to Cart", "Fast Checkout"];
 
+function filter(field) {
+    Products.orderBy(field).get().then((querySnapshot) => {
+        querySnapshot.forEach(showProducts(doc));
+    });
+};
+
 function productRedirect(id) {
     window.location = "product.html?" + id.toString();
 };
