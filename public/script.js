37b5ba768cb3b64fe17e6637931c5cfee1ce21a9
Dr_AlienTech <rdatch101@gmail.com>
2019-12-30 10:08:13 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ var db = firebase.firestore();
 db.enablePersistence();
 
 window.onload = function () {
-    if(user != null) {
+    if (user != null) {
         // RASMIT - INSERT THIS INTO THE SIGN IN REDIRECT STUFF, AND FIX REIRECTS TO ONLY CLOSE THE POPUP AND NOT ACTUALLY REDIRECT
         document.getElementById("signin").textContent = "Sign Out";
     }
