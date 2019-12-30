1e67d4c597c2401431914565a7ff2517ce79dadb
Dr_AlienTech <rdatch101@gmail.com>
2019-12-30 09:41:52 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -1,7 +1,7 @@
 var Products = db.collection("products");
 var cart = db.collection("cart").doc(user.displayName);
 
-document.
+document.onlo
 
 function search() {
     var text = document.getElementById("sample6").innerHTML;
