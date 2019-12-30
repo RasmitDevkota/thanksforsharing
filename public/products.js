eeb7b60435afd678b5426a549a9f82b3db3990a0
Dr_AlienTech <rdatch101@gmail.com>
2019-12-30 09:53:53 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -2,7 +2,7 @@ var Products = db.collection("products");
 var cart = db.collection("cart").doc(user.displayName);
 
 window.onload =  {
-
+    
 }
 
 function search() {
@@ -60,14 +60,38 @@ function filter(field) {
             var rating = doc.data().ratings.toString();
             var price = doc.data().price.toString();
 
-            function display(elem) {
-          var elem = document.getElementById(elem);
-          if (elem.style.display === "none") {
-            elem.style.display = "block";
-          } else {
-            elem.style.display = "none";
-          }
-        };
+            var outerDiv = document.createElement("div");
+            document.getElementById("results").appendChild(outerDiv);
+            outerDiv.id = "outer" + name;
+
+            var image = document.createElement("img");
+            image.src = imageRef;
+            document.getElementById(outerDiv.id).appendChild(image);
+
+            var text = document.createElement("v-text");
+            text.innerHTML = name;
+            text.className = "vtext";
+            document.getElementById(outerDiv.id).appendChild(product);
+
+            for (i = 0; i < txtElements.length; i++) {
+                var txt = txtElements[i];
+                var elem = document.createElement("v-" + txt);
+                elem.innerHTML = txt;
+                elem.className = "v" + txt;
+                document.getElementById(text.id).appendChild(elem);
+            };
+
+            var actions = document.createElement("v-actions");
+            actions.className = "vactions";
+            document.getElementById(outerDiv.id).appendChild(actions);
+
+            for (i = 0; i < actionElements.length; i++) {
+                var action = actionElements[i];
+                var elem = document.createElement("v-" + action);
+                elem.innerHTML = actionNames[i];
+                elem.classList.add("v-" + action, "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect");
+                document.getElementById(text.id).appendChild(elem);
+            };
         });
     });
 };
