95572a1a8e4a43ed8da47db8bd6b3e8d545eddeb
Dr_AlienTech <rdatch101@gmail.com>
2019-12-31 10:31:03 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -3,7 +3,7 @@ var emails = db.collection("emails");
 
 function signIn() {
     if(user == null){
-    document.getElementById('popupsignin').style.display = "block";
+        document.getElementById('popupsignin').style.display = "block";
     }
 };
 
