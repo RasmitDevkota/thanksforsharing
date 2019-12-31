8b7351f29c38e7e5a9e1be0263682845acf163ce
Dr_AlienTech <rdatch101@gmail.com>
2019-12-31 11:29:51 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -31,7 +31,7 @@ function filter(field) {
 };
 
 function results(keystring) {
-    document.getElementById(elementID).innerHTML = "";
+    document.getElementById("products").innerHTML = "";
     Products.where("keywords", "array-contains-any", keystring.split(" ")).orderBy("keywords").get().then(function (querySnapshot) {
         querySnapshot.forEach((doc) => {
             showProducts(doc);
