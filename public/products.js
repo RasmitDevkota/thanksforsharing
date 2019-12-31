e64ba3331694a3b60bdd75d26864b89fbb1aee86
Dr_AlienTech <rdatch101@gmail.com>
2019-12-31 09:04:06 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -5,7 +5,7 @@ window.onload =  function () {
 
     if (window.location.href.indexOf("products.html")) {
         var query = urlParams.get('query').toString();
-        results()
+        results();
     } else if (window.location.href.indexOf("product.html")) {
         var product = urlParams.get('product').toString();
 
