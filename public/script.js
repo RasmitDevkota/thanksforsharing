2d0b336a0f6260ef0df30543ab6e4178ae866404
Dr_AlienTech <rdatch101@gmail.com>
2019-12-31 08:41:20 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -17,7 +17,7 @@ window.onload = function () {
     if (user != null) {
         // RASMIT - INSERT THIS INTO THE SIGN IN REDIRECT STUFF, AND FIX REIRECTS TO ONLY CLOSE THE POPUP AND NOT ACTUALLY REDIRECT
         document.getElementById("signin").textContent = "Sign Out";
-        console.log("")
+        console.log("");
     } else {
         
     }
