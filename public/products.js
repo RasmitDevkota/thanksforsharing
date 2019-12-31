4313a43dda841bc29db9e509c6eee3c8a8727cf4
Dr_AlienTech <rdatch101@gmail.com>
2019-12-31 09:17:50 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -42,6 +42,7 @@ function showProducts(doc) {
     var imageRef = doc.data().imageRef.toString();
     var desc = doc.data().description.toString();
     var price = doc.data().price.toString();
+    
     var ratings = doc.data().ratings;
     var sum = 0;
 
