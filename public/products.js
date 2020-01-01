4a3e22130a7ca5a4879bedfcd5f97a46f775a455
Dr_AlienTech <rdatch101@gmail.com>
2020-01-01 00:12:05 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -69,7 +69,11 @@ function showProducts(doc) {
     text.id = "productText"
     document.getElementById(outerDiv.id).appendChild(text);
 
-    
+    var text = document.createElement("v-text");
+    text.innerHTML = name;
+    text.className = "vtext";
+    text.id = "productText"
+    document.getElementById(outerDiv.id).appendChild(text);
 
     // RASMIT - ADD STAR TO RATING
 
