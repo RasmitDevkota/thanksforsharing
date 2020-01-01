361aa936ca809d13e5e4e9e80ed76b537cd00bb5
Dr_AlienTech <rdatch101@gmail.com>
2019-12-31 22:25:33 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -88,7 +88,7 @@ function showProducts(doc) {
         if (txt == "price") {
 
         } else if{} 
-            else {
+        else {
 
         }
         elem.classList.add("v-" + action, "mdl-button", "mdl-js-button", "mdl-button--raised", "mdl-js-ripple-effect");
