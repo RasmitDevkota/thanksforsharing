710a4152ae8c3d00f44db8a6c5cb6b2260e96f34
Dr_AlienTech <rdatch101@gmail.com>
2020-01-03 17:55:40 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -107,7 +107,7 @@ function showProducts(doc) {
 
 function addToCart(productid) {
     Products.doc(productid).onSnapshot(function (doc) {
-            price = doc.data().price.toString();
+        price = doc.data().price.toString();
     });
     cart.update({
         items: firebase.firestore.FieldValue.arrayUnion(productid),
