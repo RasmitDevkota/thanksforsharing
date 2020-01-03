debf96e5f7f040f8ae607980cea0d5235174b47a
Dr_AlienTech <rdatch101@gmail.com>
2020-01-03 17:16:09 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -75,7 +75,7 @@ function showProducts(doc) {
         if (txt == "price") {
             elem.innerHTML = "$" + txtContent[i];
         } else if (txt == "rating") {
-            elem.innerHTML = "Rating: " + txtContent[i] + "<i class=></i>";
+            elem.innerHTML = "Rating: " + txtContent[i] + "<i class=''></i>";
         } else {
             elem.innerHTML = txtContent[i];
         }
