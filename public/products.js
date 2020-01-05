a087ddd085691f5fbad27be62d020b10a98f9ca0
Dr_AlienTech <rdatch101@gmail.com>
2020-01-05 10:04:53 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -90,7 +90,7 @@ function showProducts(doc) {
         if (action == "addtocart"){
             console.log(action);
             elem.addEventListener('click', function () {
-                ShoppingCart.doc(firebase.auth().currentUser.displayName).get
+                ShoppingCart.doc(firebase.auth().currentUser.displayName).get().then
                 ShoppingCart.doc(firebase.auth().currentUser.displayName).set({
                     items: firebase.firestore.FieldValue.arrayUnion(name),
                     itemCnt: firebase.firestore.FieldValue.increment(1),
