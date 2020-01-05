6c9ee3bae462f8a552f7e209df7bee6fa8a084ef
Dr_AlienTech <rdatch101@gmail.com>
2020-01-04 19:16:07 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -15,35 +15,35 @@ db.enablePersistence();
 
 window.onload = function () {
     document.getElementById("nav").innerHTML = "<!--nav begin-->
-        < nav >
-        < !--Beginning of Logo-- >
+      <nav>
+        <!--Beginning of Logo-->
         <a onclick="redirect('index.html')">Logo Here</a>
-        <!--End of Logo-- >& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;& nbsp;
+        <!--End of Logo-->&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     
-        < !--Beginning of Home-- >
+        <!--Beginning of Home-->
         <a id="home" class="navelement" onclick="redirect('index.html')">Home</a>
-        <!--End of Home-- >& nbsp;& nbsp;& nbsp;
+        <!--End of Home-->&nbsp;&nbsp;&nbsp;
     
-        < !--Beginning of Products-- >
+        <!--Beginning of Products-->
         <a id="navproducts" class="navelement" onclick="redirect('products.html?query=product')">Products</a>
-        <!--End of Products-- >& nbsp;& nbsp;& nbsp;
+        <!--End of Products-->&nbsp;&nbsp;&nbsp;
     
-        < !--Beginning of Shopping Cart-- >
+        <!--Beginning of Shopping Cart-->
         <a id="cart" class="navelement" onclick="redirect('cart.html')">Shopping Cart</a>
-        <!--End of Shopping Cart-- >& nbsp;& nbsp;& nbsp;
+        <!--End of Shopping Cart-->&nbsp;&nbsp;&nbsp;
     
-        < !--Beginning of Search-- >
+        <!--Beginning of Search-->
         <a class="navelement">
-            <i class="fas fa-search" onclick="search()"></i>
-        </a> & nbsp;
-    <input style="display: none;" id="search" class="navelement search" type="text" placeholder="Search">
+          <i class="fas fa-search" onclick="search()"></i>
+        </a>&nbsp;
+        <input style="display: none;" id="search" class="navelement search" type="text" placeholder="Search">
         <!--End of Search-->&nbsp;&nbsp;&nbsp;
-
+    
         <!--Beginning of Sign in button-->
         <a id="signin" class="navelement" onclick="signIn()">Sign In</a>
         <!--End of Sign in button-->
       </nav>
-        <!--nav end-- >"
+      <!--nav end-->"
     if (user != null) {
         // RASMIT - INSERT THIS INTO THE SIGN IN REDIRECT STUFF, AND FIX REIRECTS TO ONLY CLOSE THE POPUP AND NOT ACTUALLY REDIRECT
         document.getElementById("signin").textContent = "Sign Out";
