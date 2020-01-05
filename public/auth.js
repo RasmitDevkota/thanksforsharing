024d9437c0dbbfd1bc489e19f22a96901b630743
Dr_AlienTech <rdatch101@gmail.com>
2020-01-04 21:04:24 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -5,7 +5,7 @@ function signIn() {
     console.log(user);
     if(user == null){
         document.getElementById('popupsignin').style.display = "block";
-        document.getElementById()
+        document.getElementById();
         console.log(user);
     } else {
         firebase.auth().signOut();
