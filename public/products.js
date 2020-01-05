3d0f9ba24d9fb978601a4352e0c1487ed58eacb7
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 12:21:30 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -135,7 +135,7 @@ function showCart() {
         console.log(firebase.auth().currentUser);
         console.log(ShoppingCart.doc(firebase.auth().currentUser.displayName));
         document.getElementById("signin").innerHTML = "Sign Out";
-    } else {}
+    } else {
     ShoppingCart.doc(firebase.auth().currentUser.displayName).get().then(function (doc) {
         console.log(doc.data());
     });
