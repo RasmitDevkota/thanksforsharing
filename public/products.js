f0df6b76ebb162b70efb8b995e83f9316c48bd27
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 17:57:09 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -66,15 +66,15 @@ function showProducts(doc) {
     actions.id = "productActions" + name;
     document.getElementById(outerDiv.id).appendChild(actions);
 
-            elem.addEventListener('click', function () {
-                ShoppingCart.doc(firebase.auth().currentUser.displayName).set({
-                    items: firebase.firestore.FieldValue.arrayUnion(name),
-                    price: firebase.firestore.FieldValue.increment(price)
-                }, {merge: true});
-                showCart();
-                console.log(price);
-            });
-        document.getElementById(actions.id).appendChild(elem);
+        elem.addEventListener('click', function () {
+            ShoppingCart.doc(firebase.auth().currentUser.displayName).set({
+                items: firebase.firestore.FieldValue.arrayUnion(name),
+                price: firebase.firestore.FieldValue.increment(price)
+            }, {merge: true});
+            showCart();
+            console.log(price);
+        });
+    document.getElementById(actions.id).appendChild(elem);
 };
 
 function addToCart(productid) {
