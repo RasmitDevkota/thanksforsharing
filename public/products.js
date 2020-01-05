99532c9823c6079e8f2152f72b4402b38783b718
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 17:49:33 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -124,6 +124,74 @@ function showCart() {
     ShoppingCart.doc(firebase.auth().currentUser.displayName).get().then(function (doc) {
         console.log(doc.data());
     });
+    var name = doc.data().name.toString();
+    var imageRef = doc.data().imageRef.toString();
+    var desc = doc.data().description.toString();
+    var price = doc.data().price;
+
+    var ratings = doc.data().ratings;
+    var sum = 0;
+    for (var i = 0; i < ratings.length; i++) {
+        sum += ratings[i];
+    };
+    var rating = (sum / ratings.length).toFixed(1);
+
+    var txtContent = [name, desc, price, rating];
+
+    var outerDiv = document.createElement("v-product");
+    document.getElementById("products").appendChild(outerDiv);
+    outerDiv.id = "outer" + name;
+
+    var image = document.createElement("img");
+    image.src = imageRef;
+    document.getElementById(outerDiv.id).appendChild(image);
+
+    var text = document.createElement("v-text");
+    text.className = "vtext";
+    text.id = "productText" + name;
+    document.getElementById(outerDiv.id).appendChild(text);
+
+    for (i = 0; i < txtElements.length; i++) {
+        var txt = txtElements[i];
+        var elem = document.createElement("v-" + txt);
+        if (txt == "price") {
+            elem.innerHTML = "$" + txtContent[i] + "/month";
+        } else if (txt == "rating") {
+            elem.innerHTML = "<i class='fas fa-star'></i> " + txtContent[i];
+        } else {
+            elem.innerHTML = txtContent[i];
+        }
+        elem.className = "v" + txt;
+        document.getElementById(text.id).appendChild(elem);
+    };
+
+    var actions = document.createElement("v-actions");
+    actions.className = "vactions";
+    actions.id = "productActions" + name;
+    document.getElementById(outerDiv.id).appendChild(actions);
+
+    for (i = 0; i < actionElements.length; i++) {
+        var action = actionElements[i];
+        var elem = document.createElement("v-" + action);
+        elem.innerHTML = actionNames[i];
+        if (action == "addtocart") {
+            elem.addEventListener('click', function () {
+                ShoppingCart.doc(firebase.auth().currentUser.displayName).set({
+                    items: firebase.firestore.FieldValue.arrayUnion(name),
+                    price: firebase.firestore.FieldValue.increment(price)
+                }, { merge: true });
+                showCart();
+                console.log(price);
+            });
+        } else {
+            elem.addEventListener('click', function () {
+                console.log(action);
+                prompt("Enter Credit Card Number: ");
+            });
+        }
+        elem.classList.add("v-" + action, "mdl-button", "mdl-js-button", "mdl-button--raised", "mdl-js-ripple-effect");
+        document.getElementById(actions.id).appendChild(elem);
+    };
 };
 
 function rate(productid, val) {
