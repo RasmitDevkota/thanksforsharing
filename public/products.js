7902d900b1d184c71759a270f33a701f27709dcf
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 10:37:24 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -12,6 +12,7 @@ window.onload =  function () {
             document.getElementById("signin").textContent = "Sign Out";
         } else {
             document.getElementById('popupsignin').style.display = "block";
+            console.log("products.js ")
         }
     };
 };
