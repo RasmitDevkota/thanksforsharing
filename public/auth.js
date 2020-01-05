9e5e6cb5c88b608be1caeb25d065058b27e96828
Dr_AlienTech <rdatch101@gmail.com>
2020-01-05 09:08:44 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -84,7 +84,7 @@ function gToggleSignIn() {
                     document.getElementById("signin").textContent = "Sign Out";
 
                     var user = firebase.auth().currentUser;
-                    
+
                     console.log(user.displayName);
                     console.log(ShoppingCart);
 
@@ -147,8 +147,6 @@ function gToggleSignIn() {
                     document.getElementById("signin").textContent = "Sign In";
 
                     var user = firebase.auth().currentUser;
-
-                    var ShoppingCart = db.collection("cart").doc(user.displayName);
                     console.log(user.displayName);
                     console.log(ShoppingCart);
 
