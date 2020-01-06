4b626f070ac1469692d5598c9eaba3dceac8bf1d
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 21:11:28 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -7,7 +7,7 @@ window.onload =  function () {
         var query = urlParams.get('query');
         results(query.toString());
     } else {
-        if (firebase.auth().currentUser == null)
+        if (firebase.auth().currentUser == null) {}
         showCart();
     }
 };
