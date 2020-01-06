b7d59fd5044608f453ad88dced707679b2e71bcd
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 21:44:55 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -34,7 +34,7 @@ window.onload = function () {
 
     document.addEventListener('keydown', function (event) {
         const key = event.key;
-        if (key == "Enter" && document.getElementById()) {
+        if (key == "Enter" && document.getElementById('search')) {
             search();
         }
     });
