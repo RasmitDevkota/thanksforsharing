8b96e1075356c4a70f4315df4eea875bd2de2311
Dr AlienTech <rdatch101@gmail.com>
2020-01-06 12:50:17 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -10,7 +10,7 @@ function signIn() {
         firebase.auth().signOut();
         document.getElementById("signin").innerHTML = "Sign In";
     }
-    if (window.location.href.includes("products.html")) {
+    if (window.location.href.includes("cart.html")) {
         var urlParams = new URLSearchParams(window.location.search);
         var query = urlParams.get('query');
         results(query.toString());
