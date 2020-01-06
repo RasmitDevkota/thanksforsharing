c3c8aba2ead4daa100b69e1fcb8e08ccc8ca26ac
Dr AlienTech <rdatch101@gmail.com>
2020-01-06 10:03:08 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -130,7 +130,7 @@ function showCart() {
             nameEl.innerHTML = name;
             document.getElementById(outerDiv.id).appendChild(nameEl);
 
-            Products.
+            Products.doc()
 
             var priceEl = document.createElement("c-price");
             priceEl.className = "cprice";
