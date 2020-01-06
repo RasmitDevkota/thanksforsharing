8043af66ae6a5878f56fc60824f749540e7efc56
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 21:05:49 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -29,7 +29,7 @@ function search() {
     var text = document.getElementById("search").value.toString().toLowerCase();
     if (text == "") {
         document.getElementById('search').style.top: "10px";
-        alert("hello");
+
         /*var elem = document.getElementById("search");   
         var pos = 0;
         var id = setInterval(frame, 1000);
