5e8cde5da23d958289ea77adc47bcc654e24dd23
Dr AlienTech <rdatch101@gmail.com>
2020-01-05 20:48:55 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@ window.onload = function () {
 function search() {
     var text = document.getElementById("search").value.toString().toLowerCase();
     if (text == "") {
-        document.getElementById()
+        document.getElementById("search")
     } else {
         window.location = "products.html?query=" + text.toString();
     }
