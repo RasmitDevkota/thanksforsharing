003ab7876194e129ea55740639f4654556e07a06
Dr AlienTech <rdatch101@gmail.com>
2020-01-06 11:53:48 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -83,7 +83,7 @@ function showProducts(doc) {
                         price: price,
                         imageRef: imageRef
                     });
-                    console.log()
+                    console.lo
                 } else {
                     alert('You are currently not signed in. Sign in or use fast checkout to purchase without an account.');
                 }
