ffa70d89b72ee6c94f8116ccc3698fd11f459a38
Dr AlienTech <rdatch101@gmail.com>
2020-01-07 20:12:53 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -136,7 +136,7 @@ function showCart() {
             priceEl.innerHTML = price;
             document.getElementById(outerDiv.id).appendChild(priceEl);
 
-            var removeWrapper = document.createElement("")
+            var removeWrapper = document.createElement("removeWrapper")
             var remove = document.createElement("c-remove");
             remove.classList.add("remove", "mdl-button", "mdl-js-button", "mdl-button--icon", "mdl-button--colored");
             remove.id = "remove" + name;
