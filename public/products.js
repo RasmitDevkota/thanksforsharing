80685db6ad931842a5a4a1b074c53f06dccb9796
Dr AlienTech <rdatch101@gmail.com>
2020-01-07 21:32:01 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -124,7 +124,7 @@ function showCart() {
 
             var image = document.createElement("img");
             image.src = imageRef;
-            image.className = ""
+            image.className = "cart"
             document.getElementById(outerDiv.id).appendChild(image);
 
             var nameEl = document.createElement("c-name");
