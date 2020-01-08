1fec1c9f1b347c3a91ad676fd69f72c493faf761
Dr AlienTech <rdatch101@gmail.com>
2020-01-07 20:14:07 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -145,6 +145,7 @@ function showCart() {
             remove.id = "remove" + name;
             remove.innerHTML = " <i class='remove material-icons'>cancel</i>";
             document.getElementById(removeWrapper.id).appendChild(remove);
+            
             remove.addEventListener('click', function () {
                 ShoppingCart.doc(firebase.auth().currentUser.displayName + '/' + firebase.auth().currentUser.displayName + '/' + name).delete().then(console.log("Item removed from Cart."));
             });
