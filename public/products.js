c760f0c621c2f1688ba4859731893d67447fa002
Dr AlienTech <rdatch101@gmail.com>
2020-01-07 20:30:20 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -148,6 +148,7 @@ function showCart() {
 
             remove.addEventListener('click', function () {
                 ShoppingCart.doc(firebase.auth().currentUser.displayName + '/' + firebase.auth().currentUser.displayName + '/' + name).delete().then(function () {console.log("Item removed from Cart."))
+            
             });
             });
             document.getElementById("totalPrice").innerHTML = "Total Price: $" + totalPrice.toFixed(2);
