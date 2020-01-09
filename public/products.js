25ee5c1afc78d17945becf366ea8969aebb64309
Dr AlienTech <rdatch101@gmail.com>
2020-01-08 22:02:08 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -155,7 +155,7 @@ function showCart() {
                     totalPrice -= price
                     document.getElementById("totalPrice").innerHTML = "Total Price: $" + totalPrice.toFixed(2);
 
-                    
+                    // 
                     var rfcMsg = document.querySelector('#rfcsnackbar');
                     rfcMsg.MaterialSnackbar.showSnackbar({
                         message: 'Item removed from cart.',
