f3fd5a93d31d43de56dc2521c46e8b5d1dd451d8
Dr AlienTech <rdatch101@gmail.com>
2020-01-08 21:57:15 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -156,7 +156,7 @@ function showCart() {
                     document.getElementById("totalPrice").innerHTML = "Total Price: $" + totalPrice.toFixed(2);
 
                     var snackbarContainer = document.querySelector('#rfcsnackbar');
-                    var showSnackbarButton = document.querySelector('#' + );
+                    var showSnackbarButton = document.querySelector('#' + remove);
                     var undo = function (event) {
                         ShoppingCart.doc(firebase.auth().currentUser.displayName + '/' + firebase.auth().currentUser.displayName + '/' + name).set({
                             name: name,
