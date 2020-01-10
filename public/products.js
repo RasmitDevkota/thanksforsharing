c716713618e6624bd5486b5ae14d6a1ddbc54b97
Dr AlienTech <rdatch101@gmail.com>
2020-01-09 21:23:17 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -64,7 +64,7 @@ function showProducts(doc) {
         } else if (txt == "c2c") {
             if (c2c == true) {
                 elem.innerHTML = "<img src='c2c.png'> <p style='display: none' class='caption'>C2C</p>";
-                elem.addEventListener()
+                elem.onmouseover
             } else {
                 break;
             }
