4dd882d5d964999d0ceddf531a086228da912f3a
Dr AlienTech <rdatch101@gmail.com>
2020-01-09 21:05:45 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -60,8 +60,6 @@ function showProducts(doc) {
             elem.innerHTML = "$" + txtContent[i] + "/month for " + time;
         } else if (txt == "rating") {
             elem.innerHTML = "<i class='fas fa-star'></i> " + txtContent[i];
-        } else if (txt == "c2c" && c2c == true) {
-            elem.innerHTML = "<img src='c2c.png'>";
         } else {
             elem.innerHTML = txtContent[i];
         }
