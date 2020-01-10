a120f50b46d427ef76cb732feae95358a4d11537
Dr AlienTech <rdatch101@gmail.com>
2020-01-09 20:38:56 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -154,7 +154,6 @@ function showCart() {
                     document.getElementById(outerDiv.id).style.display = "none";
                     totalPrice -= price
                     document.getElementById("totalPrice").innerHTML = "Total Price: $" + totalPrice.toFixed(2);
-                    var affirm = true;
 
                     var rfcMsg = document.querySelector('#demo-snackbar-example');
                     'use strict';
