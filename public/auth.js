324db3c2c5d5a0345c46a5cea6cd5f3cd818a826
Dr AlienTech <rdatch101@gmail.com>
2020-01-09 19:36:12 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -30,20 +30,20 @@ function eToggleSignIn() {
     }
 
     firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
-            firebase.auth().onAuthStateChanged(function (user) {
-                display("email");
-                document.getElementById("signin").innerHTML = "Sign Out";
-            });
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
-            } else {
-                alert(errorMessage);
-            }
-            console.log(error);
+        firebase.auth().onAuthStateChanged(function (user) {
+            display("email");
+            document.getElementById("signin").innerHTML = "Sign Out";
         });
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+    });
 };
 
 // Google Login
