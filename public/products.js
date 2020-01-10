e4d8f94afdc54cec91bb2902408c60d3a3d805a8
Dr AlienTech <rdatch101@gmail.com>
2020-01-09 21:04:56 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -1,7 +1,7 @@
 var Products = db.collection("products");
 var ShoppingCart = db.collection("cart");
 
-var txtElements = ["name", "c2c", "description", "price", "rating"];
+var txtElements = ["name", "description", "price", "rating", "c2c"];
 var actionElements = ["addtocart", "checkout"];
 var actionNames = ["Add to Cart", "Fast Checkout"];
 
