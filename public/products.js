e12d5caf29a95d45e53074622c54be470e77b987
Dr AlienTech <rdatch101@gmail.com>
2020-01-09 20:27:38 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -158,6 +158,7 @@ function showCart() {
                     var showSnackbarButton = document.querySelector('#demo-show-snackbar');
                     var rfcMsg = document.querySelector('#demo-snackbar-example');
                     showSnackbarButton.addEventListener('click', function () {
+                        'use strict';
                         rfcMsg.MaterialSnackbar.showSnackbar({
                             message: 'Item removed from cart.',
                             timeout: 1700,
