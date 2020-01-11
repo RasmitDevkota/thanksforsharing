6a1b7bdac26fa2e5ecab80fd0d571666565ef2b6
Dr AlienTech <rdatch101@gmail.com>
2020-01-11 18:53:12 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -104,7 +104,7 @@ function showProducts(doc) {
             });
         } else {
             elem.addEventListener('click', function () {
-                var name = prompt()
+                var name = prompt("Enter ")
                 var ccn = prompt("Enter Credit Card Number: ");
                 var addr = prompt("Enter Shipping Address");
                 alert("Product ordered! Should arrive in around " + deliveryTime + ".");
