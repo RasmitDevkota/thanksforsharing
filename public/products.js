f4884408f3bf6a8e2eb3a664f033b4a1bbf9f658
Dr AlienTech <rdatch101@gmail.com>
2020-01-11 11:18:54 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -195,7 +195,7 @@ function checkOut() {
     alert("Product/s ordered! Each will come at their respective times, please check individual product entries for further information");
     ShoppingCart.doc(firebase.auth().currentUser.displayName).collection(firebase.auth().currentUser.displayName).get().then(function (querySnapshot) {
         querySnapshot.forEach((doc) => { 
-
+            
         })
     });
     document.getElementById("cartItems").innerHTML = "";
