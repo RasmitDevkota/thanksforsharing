80d58b6e44eb3537c7001c52f6cfa164b644e5f7
Dr AlienTech <rdatch101@gmail.com>
2020-01-11 18:50:57 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -90,7 +90,17 @@ function showProducts(doc) {
                         name: name,
                         price: price,
                         imageRef: imageRef
-                    }).then(alert("Item added to Cart."));
+                    }).then((function () {
+                        'use strict';
+                        window['counter'] = 0;
+                        var snackbarContainer = document.querySelector('#demo-toast-example');
+                        var showToastButton = document.querySelector('#demo-show-toast');
+                        showToastButton.addEventListener('click', function () {
+                            'use strict';
+                            var data = { message: 'Example Message # ' + ++counter };
+                            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+                        });
+                    }()););
                 } else {
                     alert('You are currently not signed in. Sign in or use fast checkout to purchase without an account.');
                 }
