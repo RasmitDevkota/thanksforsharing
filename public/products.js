27fcd7274825540877c627f6c47de4bc9fbfd400
Dr AlienTech <rdatch101@gmail.com>
2020-01-11 18:54:50 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -95,7 +95,7 @@ function showProducts(doc) {
                         rfcMsg.MaterialSnackbar.showSnackbar({
                             message: 'Item removed from cart.',
                             timeout: 1700,
-                            actionHandler: function (event) {
+                            actionHandler: function () {
                                 ShoppingCart.doc(firebase.auth().currentUser.displayName + '/' + firebase.auth().currentUser.displayName + '/' + name).set({
                                     name: name,
                                     price: price,
