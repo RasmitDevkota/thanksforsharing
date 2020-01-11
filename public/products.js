0faf8dc73b64b55504be40fe368766bf6d57af87
Dr AlienTech <rdatch101@gmail.com>
2020-01-11 18:55:45 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -91,7 +91,7 @@ function showProducts(doc) {
                         price: price,
                         imageRef: imageRef
                     }).then(function () {
-                        var rfcMsg = document.querySelector('#rfcMsg');
+                        var rfcMsg = document.querySelector('#');
                         rfcMsg.MaterialSnackbar.showSnackbar({
                             message: 'Item removed from cart.',
                             timeout: 1800,
