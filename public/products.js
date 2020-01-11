defc89ba89c2066c71026698fbee331c6a4477ea
Dr AlienTech <rdatch101@gmail.com>
2020-01-10 19:57:34 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -191,7 +191,7 @@ function showCart() {
 };
 
 function checkout() {
-    alert("Product ordered! Should arrive in around " + deliveryTime + ".");
+    alert("Product ordered! Should arrive in around " +  + ".");
     console.log(deliveryTime);
     ShoppingCart.doc(firebase.auth().currentUser.displayName).set({
         items: []
