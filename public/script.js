6ca7222b8a46b6d35b087dc9523af1af5e0a2c25
Dr AlienTech <rdatch101@gmail.com>
2020-01-15 21:36:01 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -17,7 +17,7 @@ var emails = db.collection("emails");
 var Products = db.collection("products");
 var ShoppingCart = db.collection("cart");
 
-firebase.auth().onAuthStateChanged
+firebase.auth().onAuthStateChanged()
 
 window.onload = function () {
     setTimeout(function () {
