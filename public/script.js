d408efa8db01ad8b7e3e69b27bdc3ba56ab68a0a
Dr AlienTech <rdatch101@gmail.com>
2020-01-15 19:42:05 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -30,7 +30,7 @@ window.onload = function () {
             showCart();
         } else if (window.location.href.includes("c2c.html")) {
             if (firebase.auth().currentUser != null) {
-                if (firebase)
+                if ()
                 document.getElementById("cartItems").style.display = "block";
             }
         } else {
