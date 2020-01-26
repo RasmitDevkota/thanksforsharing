6cad5b23d4ad1d7c658455829abb3a4d2c6e89ad
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 15:45:51 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -84,7 +84,7 @@ function removeProduct(id) {
 };
 
 function viewOrders() {
-    Orders.doc(user.displayName).get().then(function (doc) {
+    Orders.doc(user.displayName).get().then(function (querySnapshot) {
         console.log(doc.data());
     })
 };
