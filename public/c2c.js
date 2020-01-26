5007b08eb7d8672ed7f077e821cdddb0fd49e0d0
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 19:02:54 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -1,7 +1,6 @@
 // General C2C
 function c2cStart(u, usersUser) {
     usersUser = arguments[0] || false;
-    
     if (firebase.auth().currentUser != null) {
         usersUser.get().then(function (doc) {
             if (doc.data().totalPrice >= 500) {
