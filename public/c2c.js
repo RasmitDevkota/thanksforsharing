eecbfc23359302140d92b20f7323d7e19025fc77
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 16:35:43 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -89,7 +89,7 @@ function viewOrders() {
                 console.log("Order Info: ", doc.data());
             } else {
                 document.getElementById("c2c-verified-orders").innerHTML += `
-                <div class="demo-card-event mdl-card mdl-shadow--2dp">
+                    <div class="demo-card-event mdl-card mdl-shadow--2dp">
                             <div class="mdl-card__title mdl-card--expand" style="display: flex; flex-direction: column;">
                                 <h4>
                                     Product: Air Conditioner
