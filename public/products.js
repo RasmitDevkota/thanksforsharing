2dd778163b251865e8afa57f570048c0687bbb1e
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 10:00:28 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -114,7 +114,7 @@ function showProducts(doc) {
                             atcMsg.MaterialSnackbar.showSnackbar({
                                 message: 'Item already in cart',
                                 timeout: 1800,
-                                actionHandler: function () {                                },
+                                actionHandler: function () { }                                },
                                 actionText: 'Go to Cart'
                             });
                         }
