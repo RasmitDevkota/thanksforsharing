0281e629448b78a9f3e6205507b8152b80115249
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 19:57:43 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ firebase.initializeApp({
 var db = firebase.firestore();
 db.enablePersistence();
 
-var messaging = firebase.
+var messaging = firebase.messaging();
 
 var users = db.collection("users");
 var emails = db.collection("emails");
