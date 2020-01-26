a4d587385bcbb5ab43b038833b63fcde366f893d
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 11:58:42 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -264,6 +264,7 @@ function checkOut() {
         });
     }).then(function () {
         userCart.delete()
+        
         document.getElementById("cartItems").innerHTML = "<h1 style='text-align: center'>No items in cart! Head to the products page to buy something!</h1>";
         document.getElementById("totalPrice").innerHTML = "Total Price: $0.00";
     });
