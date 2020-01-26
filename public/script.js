b00edf094e0571987a20db696d206a150d01b9b1
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 21:24:34 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -44,8 +44,8 @@ firebase.auth().onAuthStateChanged(function (user) {
 
 function pageLoad(u) {
     if (u == true) {
-        document.getElementById("totalPrice").innerHTML = "Total Price: $0.00";
-        
+        document.getElementById("signin").innerHTML = "Total Price: $0.00";
+
         var user = firebase.auth().currentUser;
         var usersUser = users.doc(user.uid);
         var emailsUser = emails.doc(user.displayName);
