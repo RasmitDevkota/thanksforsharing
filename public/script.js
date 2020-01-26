d69d31497981ee636c8709859960da58f2e97272
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 14:37:37 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -43,7 +43,7 @@ function pageLoad(u) {
         window.user = firebase.auth().currentUser;
         window.usersUser = users.doc(user.uid);
         window.emailsUser = emails.doc(user.displayName);
-        window.userCart = ShoppingCart.doc(user.displayName);
+        window.userCart = ShoppingCart.doc(user.displayName).collection(user.displayName);
 
         if (window.location.href.includes("products.html")) {
             var urlParams = new URLSearchParams(window.location.search);
