c1df38fd5a1d4fe999550e7479408b9febaabce6
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 22:33:13 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -47,7 +47,7 @@ function viewProducts() {
 
             var name = doc.data().name;
 
-            var outerDiv = (
+            var product = (
                 <div class="demo-card-wide mdl-card mdl-shadow--2dp c2c-content-card">
                     <div class="mdl-card__title c2c-title">
                     <h2 class="mdl-card__title-text c2c-title-text">Product</h2>
@@ -71,7 +71,7 @@ function viewProducts() {
                     </div>
                 </div>
             );
-            
+
             ReactDOM.render(myelement, document.getElementById('c2c-verified-cards'));
         });
     });
