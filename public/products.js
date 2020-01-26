69602c14c3875b254574dd0cb589ad0bf5a5acd6
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 13:27:16 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -263,7 +263,7 @@ function checkOut() {
         });
     }).then(function () {
         var deleteFn = firebase.functions().httpsCallable('recursiveDelete');
-        deleteFn({ path: path }).then(function (result) {
+        deleteFn({ path: Sho }).then(function (result) {
             logMessage('Delete success: ' + JSON.stringify(result));
         }).catch(function (err) {
             logMessage('Delete failed, see console,');
