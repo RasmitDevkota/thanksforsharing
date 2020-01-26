67536a16238a6e5fb430e4fa187b7c7be0f1dbf5
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 15:19:14 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -13,7 +13,7 @@ function filter(field) {
 
 function results(keystring) {
     document.getElementById("products").innerHTML = "";
-    console.log(typepof keystring);
+    console.log(typeof keystring);
 
     if (keystring == "c2c") {
         Products.where("c2c", "==", true).orderBy("keywords").get().then(function (querySnapshot) {
