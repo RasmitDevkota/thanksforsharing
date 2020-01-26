2af6053d7bb62fd768c676454591a80fc3817c36
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 11:50:09 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -221,7 +221,7 @@ function showCart() {
 };
 
 function checkOut() {
-    var 
+    var coname = 
     display('copopup');
     alert("Product/s ordered! Each will come at their respective times, please check individual product entries for further information");
 
