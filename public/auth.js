1c447694f4291da2102d343fee45f3603bc00f71
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 09:22:52 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -101,7 +101,7 @@ function gToggleSignIn() {
             }
         });
     } else {
-        alert("There is already a user signed in, please sign out before proceeding.")
+        alert("There is already a user signed in, please sign out before proceeding.");
     }
 };
 // Google Login End
