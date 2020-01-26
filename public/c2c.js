2516add3fba76c08d38847b11f5e19d8c6cfe831
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 21:10:54 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -39,7 +39,7 @@ function checkVerificationStatus() {
 
 // C2C Verified
 function viewProducts() {
-    Products..get().then(function (querySnapshot) {
+    Products.doc().get().then(function (querySnapshot) {
         querySnapshot.forEach((doc) => {
             showProducts(doc);
         });
