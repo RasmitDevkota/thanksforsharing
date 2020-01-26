2dce67ed8916874711c52362fa7ef23e4b24c3b9
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 16:09:27 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -172,7 +172,7 @@ function showCart() {
             nameEl.id = "cartName" + name;
             if (c2c == true) {
                 var seller = doc.data().c2cauthor;
-                nameEl.innerHTML = name + `<img src='c2c.png' title='Sold by ${seller}'>`;
+                nameEl.innerHTML = name + `<img src='c2c.png' title='C2C - Sold by ${seller}'>`;
             } else {
                 nameEl.innerHTML = name;
             }
