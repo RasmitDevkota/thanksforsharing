b36863e098a01e22685ee16f10eebc34a37bf7f6
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 11:47:10 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -235,7 +235,7 @@ function checkOut() {
                 var seller = doc.data().c2c - author;
                 var name = doc.data().name;
                 
-                Orders.doc(seller + '/' + user.displayName + '/')
+                Orders.doc(seller + '/' + user.displayName + '/' + info)
                 Orders.doc(seller + '/' + user.displayName + '/' + name).set({
                     name: name
                 });
