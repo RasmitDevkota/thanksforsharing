17e7c8687a5dc3054c53c249f5fc9ca00489e54a
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 23:01:14 -0500

    Gitacora saved 3 changed files

--- public/script.js
+++ public/script.js
@@ -28,6 +28,9 @@ window.onload = function () {
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
+
+
+        $(document).ready(function () {
         pageLoad();
     } else {
         console.log("Signed out");
