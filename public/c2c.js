cca7f8e7435d7bb9f1ede48deb1b4aa4f4729adb
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 15:56:09 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -83,7 +83,7 @@ function removeProduct(id) {
 };
 
 function viewOrders() {
-    var userOrders = db.collectionGroup(user).where('type', '==', 'museum');
+    var userOrders = db.collectionGroup(user.displayName).where('type', '==', 'museum');
     userOrders.get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
 
