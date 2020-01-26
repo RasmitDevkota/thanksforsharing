9deb5a35b2d6d1940351c2196c9b5e208ab1124e
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 11:55:01 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -254,7 +254,7 @@ function checkOut() {
                               zipcode: cozipcode
                           });
                       }  
-                    })
+                    });
                 });
             }
         }).then(function () {
