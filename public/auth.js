541f59f65b180400e936eb6cccd7f6fcc7bd967c
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 09:20:44 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -71,7 +71,7 @@ function gToggleSignIn() {
                             } else {
                                 console.log("Emails doc already exists, skipped writing.");
                             }
-                        }).then;
+                        }).then();
 
                         userDataUsers.get().then(function (doc) {
                             if (!doc.exists) {
