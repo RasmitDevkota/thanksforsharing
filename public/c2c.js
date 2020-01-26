009261edb74fb227a29022b1804547a28336eca0
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 16:43:11 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -86,7 +86,6 @@ function viewOrders() {
     db.collectionGroup(user.displayName).get().then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
             var name = doc.data().name;
-            
 
             document.getElementById("c2c-verified-orders").innerHTML += `
                 <div id="${name}" class="demo-card-event mdl-card mdl-shadow--2dp">
