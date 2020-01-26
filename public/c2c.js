3048f811712e3c4b0b673bd6c12944ffa5aaf40d
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 16:45:09 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -99,7 +99,7 @@ function viewOrders() {
                             <a id="hide-details-${buyer}-${name}" style="display: none;" onclick="display('details'); display('show-details'); display('hide-details')">Hide</a>
                         </h4>
                         <h3 id="details-${buyer}-${name}" style="display: none;">
-                            ${buyer}${buyer}${buyer}<br>
+                            ${buyer}${buyer} ${buyer}<br>
                             ${buyer}, ${buyer} ${buyer}<br>
                         </h3>
                     </div>
