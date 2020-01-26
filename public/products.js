aa517c70672fffb2629f6b35e0b3e83e08ac8a1a
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 15:15:50 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -19,7 +19,7 @@ function results(keystring) {
         querySnapshot.forEach((doc) => {
             showProducts(doc);
         });
-        console
+        console(keystring)
     });
 
     if (keystring == "c2c") {
