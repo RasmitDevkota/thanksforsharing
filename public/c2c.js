0ecd08c1f6ea7d3b3a31f8c257b08a959dd49f31
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 21:59:03 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -58,11 +58,11 @@ function viewProducts() {
                         <i class="material-icons">delete</i>
                     </a>
                 </div>
-                <!-- < div class="mdl-card__menu" >
+                < div class="mdl-card__menu" >
                     <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
                         <i class="material-icons">open_in_new</i>
                     </button>
-                </div > -->
+                </div >
 
             var outerDiv = "<div class='demo-card-wide mdl-card mdl-shadow--2dp c2c-content- card'></div>";
             $('#c2c-verified-cards').append(outerDiv);
