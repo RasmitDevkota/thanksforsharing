1b6e5c4426644c8d11c7a78fefd9c73b18f5f176
Dr AlienTech <rdatch101@gmail.com>
2020-01-25 21:59:45 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -67,7 +67,7 @@ function viewProducts() {
             var outerDiv = "<div class='demo-card-wide mdl-card mdl-shadow--2dp c2c-content- card'></div>";
             $('#c2c-verified-cards').append(outerDiv);
 
-            var title = 
+            var title = "<div class="mdl-card__title c2c - title">"
         });
     });
 };
