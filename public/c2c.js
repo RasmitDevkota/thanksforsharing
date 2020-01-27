dab541bad3a6d4f3a3a94e3eca19def7de223411
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 20:41:50 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -123,6 +123,7 @@ function addProduct() {
     var name = document.getElementById("adddesc").value;
     var name = document.getElementById("addrent").value;
     var name = document.getElementById("addname").value;
+    var name = document.getElementById("addname").value;
 
     userDataEmails.get().then(function (doc) {
         if (!doc.exists) {
