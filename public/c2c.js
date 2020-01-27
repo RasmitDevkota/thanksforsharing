082a7855e00db117d35077210c5628915918f10b
Dr AlienTech <rdatch101@gmail.com>
2020-01-26 20:44:34 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -130,7 +130,7 @@ function addProduct() {
     Products.doc(name).get().then(function (doc) {
         if (!doc.exists) {
             doc.set({
-                name: email,
+                name: name,
                 uid: uid,
             }).then(function () {
                 console.log("Document successfully written!");
