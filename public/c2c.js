71c7ee624cd2ecba2759cb83dc0aa7a9480c1300
Dr AlienTech <rdatch101@gmail.com>
2020-01-27 22:37:18 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -122,6 +122,7 @@ function addProduct() {
     var rent = document.getElementById("addrent").value;
     var price = document.getElementById("addprice").value;
     var keywordsStr = document.getElementById("keywords").value;
+    var keywords = key
     var period = document.getElementById("addperiod").value;
     var t = document.getElementById("timestamp");
     var timestamp = t.options[t.selectedIndex].value;
