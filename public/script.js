02f2fa4d8c8f7ba1920b8f1af16ec13984275139
Dr AlienTech <rdatch101@gmail.com>
2020-01-27 22:40:56 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -56,8 +56,7 @@ function pageLoad(u) {
             usersUser.get().then(function (doc) {
                 if (doc.data().totalPrice >= 500) {
                     console.log("c2c-verified");
-                    document.getElementById("c2c-verified").style.display = "block";
-                    viewProducts();
+                    c2cStart
                 } else {
                     console.log("c2c-unverified");
                     document.getElementById("c2c-unverified").style.display = "block";
