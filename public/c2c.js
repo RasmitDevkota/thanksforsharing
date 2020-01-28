7f4bdd85b6dc7ca061c15ef3a486ce4ef8540db4
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 18:49:38 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -7,7 +7,7 @@ function c2cStart() {
             if (doc.data().totalPrice >= 500) {
 
                 askCNN();
-                if (askC.length != 16) {
+                if (askCNN().length != 16) {
                     alert('Please enter a valid credit card number without spaces!');
                     location.reload();
                 } else {
