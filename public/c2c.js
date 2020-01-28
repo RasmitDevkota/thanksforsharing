ca63eb8760b65c6908cd201639b476c01aac9704
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 18:34:22 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -15,7 +15,7 @@ function c2cStart() {
             if (doc.data().paymentInformation == null) {
                 cc = prompt('Please enter the credit card number you would like to use with C2C.');
                 if (cc.length() > 16) {
-                    
+                    alert()
                 }
                 usersUser.set({
                     paymentInformation: cc
