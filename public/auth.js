b61c11b0185f67a2c1ecae471d3e5d862c13789e
Dr AlienTech <rdatch101@gmail.com>
2020-01-27 22:09:58 -0500

    Gitacora saved 1 changed files

--- public/auth.js
+++ public/auth.js
@@ -159,18 +159,6 @@ function handleSignUp() {
                     pageLoad(true);
                 });
             });
-
-            user.updateProfile({
-                displayName: permusername,
-            }).then(function () {
-                console.log(user.displayName);
-            }).catch(function (error) {
-                console.log(error);
-                console.log(user.displayName);
-            }).then(function () {
-                display('signup');
-                pageLoad(true);
-            });
         });
     }).catch(function (error) {
         var errorCode = error.code;
