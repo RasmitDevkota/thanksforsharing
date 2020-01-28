3c426943fcc0f3fc49c3fe7953305ca0e8a19b8f
Dr AlienTech <rdatch101@gmail.com>
2020-01-27 22:33:42 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -148,7 +148,7 @@ function addProduct() {
 };
 
 function verifyOrder(id) {
-    var c = confirm("Confirm that this product has been shipped by the seller (not necessarily received by the buyer) and that payment has been received?");
+    var c = confirm("Confirm that this product has been shipped by the seller (not necessarily received by the buyer) and that payment has been received? (Note: ");
     if (c == true) {
         // $('#order-' + id).remove();
     }
