0b12612dde84e3d9cd7e8e3da10b9cc00e96bd73
Dr AlienTech <rdatch101@gmail.com>
2020-01-27 22:53:24 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -21,7 +21,7 @@ var Orders = db.collection("orders");
 document.addEventListener('keydown', function (event) {
     const key = event.key;
     if (key == "Enter") {
-
+        if()
         search();
     } else if (document.getElementById('popupsignin').style.display == "block") {
 
