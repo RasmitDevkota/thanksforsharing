1b5b0eec04cd41c8f8fff07da16f96ce029c4b93
Dr AlienTech <rdatch101@gmail.com>
2020-01-27 22:38:53 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -136,7 +136,7 @@ function addProduct() {
                 description: desc,
                 price: price,
                 time: time,
-                keywords: []
+                keywords: [key]
                 ratings: [5],
                 imageRef: imageRef,
                 deliveryTime: "5 days",
