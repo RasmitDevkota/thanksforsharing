4f7773f766514b48e268bbcb5b9f27a44c7e8990
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 21:51:29 -0500

    Gitacora saved 1 changed files

--- functions/index.ts
+++ functions/index.ts
@@ -3,3 +3,7 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
+
+export const addNumbers = functions.https.onRequest((request, response) => {
+    response.send(Number(request.query.a) + Number(request.query.b));
+});
