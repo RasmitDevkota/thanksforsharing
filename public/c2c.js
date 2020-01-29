4245b52acce21bf644a7e06b585b810e70a0d1ae
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 20:01:21 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -82,8 +82,8 @@ function viewOrders() {
                 <div id="order-${buyer}-${name}" class="demo-card-event mdl-card mdl-shadow--2dp c2c-content-card">
                     <div class="mdl-card__title mdl-card--expand info" style="display: flex; flex-direction: column;">
                         <h5 class="info">
-                            Product: ${name}<br><br><br>
-                            Buyer: ${buyer}<br><br><br><br>
+                            Product: ${name}<br><br>
+                            Buyer: ${buyer}<br><br>
                             ${addr}<br><br>
                             ${city}, ${state} ${zipcode}
                         </h5>
