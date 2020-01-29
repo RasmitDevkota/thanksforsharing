f284cbc25ede64af5b7a38fdd533fcb60a01318d
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 20:07:53 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -144,7 +144,6 @@ function verifyOrder(id, buyer) {
     var c = confirm("Confirm that this product has been shipped by the seller (not necessarily received by the buyer) and that payment has been received?");
     console.log(id);
     if (c) {
-        console.log(document.getElementById('order-' + id));
         db.collectionGroup(user.displayName).where("productName", "==", id).where("name", "==", buyer).get().then(function (querySnapshot) {
             querySnapshot.forEach(function (doc) {
                 console.log(doc.data().productName);
