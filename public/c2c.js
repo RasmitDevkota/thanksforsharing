7a7770939807eaf3cb48e86dd0dbbc4dcb2996de
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 19:34:03 -0500

    Gitacora saved 1 changed files

--- public/c2c.js
+++ public/c2c.js
@@ -91,7 +91,7 @@ function viewOrders() {
                     </div>
                     <div class="mdl-card__actions mdl-card--border verifyButton">
                         <div class="mdl-layout-spacer"></div>
-                        <i class="material-icons mdl-button mdl-js-button mdl-js-ripple-effect" onclick="verifyOrder('${buyer}-${name}, ${}')" style="cursor: pointer;">check</i>
+                        <i class="material-icons mdl-button mdl-js-button mdl-js-ripple-effect" onclick="verifyOrder('${buyer}-${name}, ${name}')" style="cursor: pointer;">check</i>
                         <div class="mdl-layout-spacer"></div>
                     </div>
                 </div>
