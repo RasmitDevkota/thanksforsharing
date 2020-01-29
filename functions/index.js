38444870c5b52d93504fc7ecdf1101d94b1d978c
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 21:49:03 -0500

    Gitacora saved 2 changed files

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,6 @@ import functions from 'firebase-functions';
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
