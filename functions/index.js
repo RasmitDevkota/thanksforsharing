bae18665699eada0b72fe968471a59c303453bb5
Dr AlienTech <rdatch101@gmail.com>
2020-01-28 21:48:53 -0500

    Gitacora saved 2 changed files

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 import functions from 'firebase-functions';
 
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
