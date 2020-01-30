e8d99d77766e99c1bc529de5b97ba9c694cc7217
Dr AlienTech <rdatch101@gmail.com>
2020-01-30 18:27:30 -0500

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -230,7 +230,7 @@ function showCart() {
 };
 
 function showProducts(name) {
-    Products.where("name", "in", ).get().then(function (doc) {
+    Products.where("name", "==", name).get().then(function (doc) {
         var name = doc.data().name.toString();
         var imageRef = doc.data().imageRef.toString();
         var desc = doc.data().description.toString();
