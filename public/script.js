ea09eb1345e6f465e363fb5134c1ae2af575a87f
Dr AlienTech <rdatch101@gmail.com>
2020-01-30 20:52:29 -0500

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -123,7 +123,7 @@ function toggleSlideMenu() {
     if (document.getElementById('slideinmenu').style.display == "none") {
         $('#slideinmenu').show();
         $("#slideinmenu").animate({
-            left: '0.015%',
+            right: '0.015%',
         });
     } else {
         $("#slideinmenu").animate({
