a20fe57e473075cd726d45805c1fe45d8f3f650f
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-03 16:43:20 -0400

    Gitacora saved 1 changed files

--- public/script.js
+++ public/script.js
@@ -84,7 +84,7 @@ function pageLoad(u) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("demo").innerHTML = this.responseText;
+                document.getElementById("replacethiselement").innerHTML = this.responseText;
             }
         };
         xhttp.open("GET", "templates.html", true);
