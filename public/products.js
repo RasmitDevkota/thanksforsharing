110a773c6aec2c760e91caf50055a2b19c907e2a
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-05 18:42:42 -0400

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -6,7 +6,6 @@ function filter(field) {
     document.getElementById("products").innerHTML = "";
     Products.orderBy(field).get().then(function (querySnapshot) {
         querySnapshot.forEach((doc) => {
-                console.log(doc);
             showProducts(doc);
         });
     });
@@ -18,21 +17,18 @@ function results(keystring) {
     if (keystring == "c2c") {
         Products.where("c2c", "==", true).orderBy("keywords").get().then(function (querySnapshot) {
             querySnapshot.forEach((doc) => {
-                console.log(doc);
                 showProducts(doc);
             });
         });
     } else {
         Products.where("keywords", "array-contains-any", keystring.split(" ")).orderBy("keywords").get().then(function (querySnapshot) {
             querySnapshot.forEach((doc) => {
-                console.log(doc);
                 showProducts(doc);
             });
         });
 
         Products.where("name", "in", keystring.split(" ")).get().then(function (querySnapshot) {
             querySnapshot.forEach((doc) => {
-                console.log(doc);
                 showProducts(doc);
             });
         });
@@ -40,7 +36,7 @@ function results(keystring) {
 };
 
 function updateTimestamp(docRef) {
-    console.log(docRef);
+    console.log(docRef)
     docRef.update({
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
     });
