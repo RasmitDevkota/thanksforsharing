5fbbdb693b9264192e4e468c1b7c3eff3b028087
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-05 18:49:11 -0400

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -30,6 +30,7 @@ function results(keystring) {
 
         Products.where("name", "in", keystring.split(" ")).get().then(function (querySnapshot) {
             querySnapshot.forEach((doc) => {
+                console.log(doc == Products.doc(doc.id));
                 showProducts(doc);
             });
         });
