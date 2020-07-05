7b84276cae69ebe691150855ea6c7a47760d5e7e
Dr AlienTech <alientechgamig@hotmail.com>
2020-07-05 18:40:14 -0400

    Gitacora saved 1 changed files

--- public/products.js
+++ public/products.js
@@ -49,8 +49,8 @@ function showProducts(docc) {
     var name = docc.data().name.toString();
     var imageRef = docc.data().imageRef.toString();
     var desc = docc.data().description.toString();
-    var price = doc.data().price;
-    var time = doc.data().time.toString();
+    var price = docc.data().price;
+    var time = docc.data().time.toString();
     var deliveryTime = doc.data().deliveryTime.toString();
     var c2c = doc.data().c2c;
 
